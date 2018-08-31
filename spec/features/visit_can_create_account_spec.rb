2b99a5ed54883c02aa1025e1179ecc75a4ba8b0b
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-31 08:43:13 -0600

    Adds test for new user account

---
+++ spec/features/visit_can_create_account_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "As a visitor" do
+  describe "when I visit '/' " do
+    it 'I should be able to create an account' do
+      username = "mommakat"
+      visit '/'
+      click_link("Create Account")
+
+      expect(current_path).to eq(new_user_path)
+      fill_in :user_usernmae, with: username
+      fill_in :user_password, with: "test"
+
+      click_on("Create User")
+
+      expect(page).to have_content("Welcome, #{username}!")
+    end
+  end
+end
