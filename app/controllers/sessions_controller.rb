6e9f8299991b31417eb5efca0691a081bb50124b
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-01 10:59:04 -0600

    Tries to correct tests and layouts for a logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(username: params[:login][:username])
+    if user && user.authenticate(params[:login][:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
