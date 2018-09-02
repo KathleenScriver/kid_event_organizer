851f573f39a8ed946be5b0d543e5173bb0cddf5d
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-02 13:33:16 -0600

    moves create new kid functions to admin

--- app/controllers/admin/kids_controller.rb
+++ app/controllers/admin/kids_controller.rb
@@ -2,4 +2,22 @@ class Admin::KidsController < ApplicationController
   def index
     @kids = Kid.all
   end
+
+  def new
+    @kid = Kid.new()
+  end
+
+  def create
+    kid = Kid.create(kid_params)
+    redirect_to admin_kid_path(kid)
+  end
+
+  def show
+    @kid = Kid.find(params[:id])
+  end
+
+  private
+    def kid_params
+      params.require(:kid).permit(:name)
+    end
 end
--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -5,21 +5,7 @@ class KidsController < ApplicationController
     @kids = Kid.all
   end
 
-  def new
-    @kid = Kid.new()
-  end
-
-  def create
-    kid = Kid.create(kid_params)
-    redirect_to kid_path(kid)
-  end
-
   def show
     @kid = Kid.find(params[:id])
   end
-
-  private
-    def kid_params
-      params.require(:kid).permit(:name)
-    end
 end
