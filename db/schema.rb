4c1fc8e4306ecde5b388d4e85bc4b9a19113b5b0
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-02 12:31:51 -0600

    Adds roles to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180901210521) do
+ActiveRecord::Schema.define(version: 20180902183049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180901210521) do
     t.string "title"
     t.string "username"
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "events", "kids"
