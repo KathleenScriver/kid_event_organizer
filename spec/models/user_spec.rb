3551686b01a58e8ea7ed1d254f775d1ae8bc811e
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-02 12:19:32 -0600

    Adds test for uniquness of username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,13 @@ describe User, type: :model do
     it {should validate_presence_of(:username)}
     it {should validate_uniqueness_of(:username)}
     it {should validate_presence_of(:password)}
+
+    it "should have unique username" do
+      first = User.create(name: "kat", title: "mom", username: "kat123", password: "test")
+      second = User.create(name: "kat", title: "mom", username: "kat123", password: "test")
+
+      expect(second).to_not be_valid
+    end
   end
 
   describe 'relationships' do
