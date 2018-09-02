f8a4f6c23e48b9b5cfefb42d1ea203d89149d07e
Kathleen Yruegas <kathleenecn@gmail.com>
2018-09-02 12:43:35 -0600

    Corrects schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902183049) do
+ActiveRecord::Schema.define(version: 20180902184147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
